7c391c91caa6fdc7824e2124d74b024a84f748f0
unknown <genzr05@gmail.com>
2024-01-27 19:42:44 +1000

    some cleanup

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -68,7 +68,6 @@ internal void Win32RenderWeirdGradient(win32_offscreen_buffer Buffer, int BlueOf
 
 internal void Win32ResizeDIBSection(win32_offscreen_buffer* Buffer, int Width, int Height)
 {
-    
     if (Buffer->Memory)
     {
 		VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
@@ -92,7 +91,6 @@ internal void Win32ResizeDIBSection(win32_offscreen_buffer* Buffer, int Width, i
 
     int BitmapMemorySize = (Buffer->Width* Buffer->Height)*Buffer->BytesPerPixel;
     Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
-    
 }
 
 internal void Win32DisplayBufferInWindow(win32_offscreen_buffer Buffer, HDC DeviceContext, int X, int Y, int Width, int Height)
@@ -114,60 +112,55 @@ LRESULT CALLBACK MainWindowCallback(
     WPARAM WParam,
     LPARAM LParam)
 {
-
     LRESULT Result = 0;
 
     switch (Message)
     {
-    case WM_SIZE:
-    {
-        
-        win32_window_dimensions Dimensions = Win32GetWindowDimension(Window);
-        Win32ResizeDIBSection(&GlobalBackBuffer, Dimensions.Width, Dimensions.Height);
+        case WM_SIZE:
+        {
+            win32_window_dimensions Dimensions = Win32GetWindowDimension(Window);
+            Win32ResizeDIBSection(&GlobalBackBuffer, Dimensions.Width, Dimensions.Height);
 
-        OutputDebugStringA("WM_SIZE\n");
-    } break;
-    case WM_DESTROY:
-    {
-        Running = false;
-        OutputDebugStringA("WM_DESTROY\n");
-    } break;
-    case WM_ACTIVATEAPP:
-    {
-        OutputDebugStringA("WM_ACTIVATEAPP\n");
-    } break;
-    case WM_CLOSE:
-    {
-        Running = false;
-        OutputDebugStringA("WM_CLOSE\n");
-    } break;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT Paint;
-        HDC DeviceContext = BeginPaint(Window, &Paint);
-        int X = Paint.rcPaint.left;
-        int Y = Paint.rcPaint.top;
-        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        case WM_DESTROY:
+        {
+            Running = false;
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+        case WM_CLOSE:
+        {
+            Running = false;
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 
-
-        Win32DisplayBufferInWindow(GlobalBackBuffer, DeviceContext, X, Y, Width, Height);
-        EndPaint(Window, &Paint);
-        OutputDebugStringA("WM_PAINT\n");
-    } break;
-    default:
-    {
-        Result = DefWindowProc(Window, Message, WParam, LParam);
-    } break;
+            Win32DisplayBufferInWindow(GlobalBackBuffer, DeviceContext, X, Y, Width, Height);
+            EndPaint(Window, &Paint);
+            OutputDebugStringA("WM_PAINT\n");
+        } break;
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
     }
-
     return Result;
 }
 
 int APIENTRY WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, int ShowCode)
 {
     WNDCLASSA WindowClass = {};
-   
     // TODO -  Check if redraws are necessary
     WindowClass.style = CS_HREDRAW|CS_VREDRAW;
     WindowClass.hInstance = Instance;
@@ -224,7 +217,6 @@ int APIENTRY WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLin
                 XOffset++;
                 YOffset++;
             }
-
         }
         else
         {
@@ -235,8 +227,6 @@ int APIENTRY WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLin
     {
         //TODO - Logging
     }
-
-
 
     return 0;
 }
